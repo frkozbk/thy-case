dba97adf7bdbed27af9592430ca9d603e61dfee6
frk_ozbk <ozbekfaruk12@gmail.com>
2024-07-20 11:29:13 +0300

    FE-0 fixed typo

--- __tests__/CabinElement.test.tsx
+++ __tests__/CabinElement.test.tsx
@@ -3,7 +3,7 @@ import { CabinElement } from "../components/PlaneSeatLayout/CabinElement"
 import { CabinElements } from "@/constants/CabinElements"
 import { ICabinElement } from "@/types/SeatMapData.types"
 
-describe("CabinElement", () => {})
+describe("CabinElement", () => {
   test("renders a meal icon when type is MEAL", () => {
     const cabinElement: ICabinElement = {
       rowNumber: "0",
@@ -60,7 +60,7 @@ describe("CabinElement", () => {})
     expect(doorElement).toBeInTheDocument()
   })
 
-  it("renders a seat component when type is SEAT", () => {
+  it("renders a standart-seat icon when the seat is reservered", () => {
     const cabinElement: ICabinElement = {
       rowNumber: "0",
       type: CabinElements.SEAT,
@@ -69,10 +69,132 @@ describe("CabinElement", () => {})
       wcFlag: false,
       exitCorridor: false,
       wcSeatFlag: true,
+      planeSeat: {
+        seatNumber: "02C",
+        nonSmoking: false,
+        smoking: false,
+        exit: false,
+        basinet: false,
+        aisle: true,
+        window: false,
+        center: false,
+        notForMedical: false,
+        notForYoung: false,
+        notForInfant: true,
+        notForChild: false,
+        notForPetc: false,
+        forTransit: false,
+        largeSpace: false,
+        chargableSeat: false,
+        leastPreferable: false,
+        handicap: false,
+        selectable: false,
+        available: false,
+        standard: true,
+        reserved: false,
+        seatType: "CORRIDOR",
+        rightSideExtraSeatAvailable: false,
+        leftSideExtraSeatAvailable: false,
+        leftSideExtraSeatNumber: null,
+        rightSideExtraSeatNumber: null,
+        planeSeatAmenity: null,
+        planeSeatAmenities: [
+          {
+            description: "NO FARE",
+            currency: null,
+            code: "Non-Chargable Seat",
+            amount: null,
+            mileAmount: null,
+            mileCurrency: null,
+            passengerRph: "1",
+            premiumCode: "1",
+            feeCode: null,
+            canSelectSeat: true,
+            mandatoryInd: null,
+            beforeDiscountAmount: null,
+            beforeDiscountMileAmount: null,
+          },
+        ],
+        mandatoryInd: null,
+        planeSection: null,
+        premiumCode: null,
+        occupied: false,
+      },
     }
     render(<CabinElement cabinElement={cabinElement} />)
 
-    const seatComponent = screen.getByTestId("seat-icon")
+    const standardSeatIcon = screen.getByTestId("icon-standart-seat");
 
-    expect(seatComponent).toBeInTheDocument()
+    expect(standardSeatIcon).toBeInTheDocument();
+
   })
+  it("renders a full-seat icon when the seat is reservered", () => {
+    const cabinElement: ICabinElement = {
+      rowNumber: "0",
+      type: CabinElements.SEAT,
+      elementString: null,
+      seatLocationType:  CabinElements.SEAT,
+      wcFlag: false,
+      exitCorridor: false,
+      wcSeatFlag: true,
+      planeSeat: {
+        seatNumber: "02C",
+        nonSmoking: false,
+        smoking: false,
+        exit: false,
+        basinet: false,
+        aisle: true,
+        window: false,
+        center: false,
+        notForMedical: false,
+        notForYoung: false,
+        notForInfant: true,
+        notForChild: false,
+        notForPetc: false,
+        forTransit: false,
+        largeSpace: false,
+        chargableSeat: false,
+        leastPreferable: false,
+        handicap: false,
+        selectable: false,
+        available: false,
+        standard: false,
+        reserved: true,
+        seatType: "CORRIDOR",
+        rightSideExtraSeatAvailable: false,
+        leftSideExtraSeatAvailable: false,
+        leftSideExtraSeatNumber: null,
+        rightSideExtraSeatNumber: null,
+        planeSeatAmenity: null,
+        planeSeatAmenities: [
+          {
+            description: "NO FARE",
+            currency: null,
+            code: "Non-Chargable Seat",
+            amount: null,
+            mileAmount: null,
+            mileCurrency: null,
+            passengerRph: "1",
+            premiumCode: "1",
+            feeCode: null,
+            canSelectSeat: true,
+            mandatoryInd: null,
+            beforeDiscountAmount: null,
+            beforeDiscountMileAmount: null,
+          },
+        ],
+        mandatoryInd: null,
+        planeSection: null,
+        premiumCode: null,
+        occupied: false,
+      },
+    }
+    render(<CabinElement cabinElement={cabinElement} />)
+
+    const standardSeatIcon = screen.getByTestId("icon-full-seat");
+
+    expect(standardSeatIcon).toBeInTheDocument();
+
+  })
+})
+
